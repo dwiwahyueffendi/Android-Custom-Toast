ee772ac1a1d5b64de9acfa917c92feb762bc042d
DwiWahyuEffendi <dwiwahyueffendi22@gmail.com>
2020-11-23 20:32:16 +0700

    Connect Activity

--- app/src/main/java/com/example/androidcustomtoast/MainActivity.kt
+++ app/src/main/java/com/example/androidcustomtoast/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.androidcustomtoast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonGeneralToast.setOnClickListener(){
+
+        }
+    }
+
+    private fun showToast(){
+
     }
 }
