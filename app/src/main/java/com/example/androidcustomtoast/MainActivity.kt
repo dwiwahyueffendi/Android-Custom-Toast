f198853953e14daad1d532f8074f2d8e6ed92a43
DwiWahyuEffendi <dwiwahyueffendi22@gmail.com>
2020-11-24 20:37:09 +0700

    Connect ic_toast to activity

--- app/src/main/java/com/example/androidcustomtoast/MainActivity.kt
+++ app/src/main/java/com/example/androidcustomtoast/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.androidcustomtoast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,17 +13,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        buttonGeneralToast.setOnClickListener(){
-            showToast()
-        }
     }
 
-    private fun showToast(){
+    private fun showToast(toastType: Int){
         val toastView = layoutInflater.inflate(
                 R.layout.layout_toast,
                 findViewById(R.id.layoutToastContainer)
         )
+
+        val toastContainer = toastView.findViewById<LinearLayout>(R.id.layoutToastContainer)
+        val imageIcon = toastView.findViewById<ImageView>(R.id.imageIcon)
+        val textMessage = toastView.findViewById<TextView>(R.id.textMessage)
 
         with(Toast(applicationContext)){
             duration = Toast.LENGTH_SHORT
